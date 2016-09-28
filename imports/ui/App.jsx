05d26292fbf24ad66ae92871c77e0c5a8bfeaf26
thibault-p <thibault.person@gmail.com>
2016-09-28 21:59:31 +0200

    First UI draft

---
+++ imports/ui/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+// import Rooms from './Rooms.jsx';
+import AppBar from './AppBar.jsx';
+
+
+export default class App extends Component {
+
+
+  render() {
+    return  (
+      <div className="container-fluid">
+        <AppBar />
+      </div>
+    );
+  }
+}
---
+++ imports/ui/AppBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default class AppBar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="#">Snoop</a>
+        </div>
+      </nav>
+    );
+  }
+}
